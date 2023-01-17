a56c9c68b7faf74663c1929317d9874c9c1f2efa
krnpv666 <122897100+krnpv666@users.noreply.github.com>
2023-01-17 23:02:55 +0800

    Create APPX.php

---
+++ APPX.php
@@ -0,0 +1,5 @@
+[{"verCode":"2",
+  "verName":"2.0",
+  "appUrl":"https://github.com/RocksGenes/krv-api/raw/main/krnpv.apk",
+  "upNotes":"~New Improvement System\n~~~~~~~~~~~~~~\nNotes : If update button is not working in your phone, go to https://www.google.com to download the latest app."
+}]
