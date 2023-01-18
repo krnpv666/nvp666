98c8b3de320f35d6ec2f5066d3e4cb5c0e5f337f
krnpv666 <122897100+krnpv666@users.noreply.github.com>
2023-01-18 13:58:35 +0800

    Update APPX.php

--- APPX.php
+++ APPX.php
@@ -1,5 +1,5 @@
 [{"verCode":"1",
   "verName":"1.0.0",
-  "appUrl":"https://github.com/krnpv666/nvp666/raw/main/krnpv.apk",
+  "appUrl":"https://github.com/krnpv666/nvp666/raw/main/KoroVPN1.0.0",
   "upNotes":"~New UI Design and UPDATED Servers!\n~~~~~~~~~~~~~~\nNotes : If update button is not working in your phone, go to https://bit.ly/m/korovpn to download the latest app."
 }]
