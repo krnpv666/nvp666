7d4703cee8bb68d1ca721a5f7a809fdb2c87db70
krnpv666 <122897100+krnpv666@users.noreply.github.com>
2023-01-29 02:58:17 +0800

    Update ICSKORO.php

--- ICSKORO.php
+++ ICSKORO.php
@@ -1,5 +1,5 @@
 [{"verCode":"2",
   "verName":"2.0.0",
-  "appUrl":"https://github.com/krnpv666/nvp666/raw/main/KoroVPN1.0.0.apk",
+  "appUrl":"https://github.com/krnpv666/nvp666/raw/main/app-release.apk",
   "upNotes":"~New UI Design and UPDATED Servers!\n~~~~~~~~~~~~~~\nNotes : If update button is not working in your phone, go to https://bit.ly/m/korovpn to download the latest app."
 }]
